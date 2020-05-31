f5a40de5abc16a1e9e7ce0da322036d7cb91d525
Afej <afekurojoshua@gmail.com>
2020-05-31 15:58:43 +0100

    worked on image optimization

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// Sticky menu background
-// window.addEventListener('scroll', function () {
-//   if (window.scrollY > 150) {
-//     document.querySelector('#main-nav').style.opacity = 0.9;
-//   } else {
-//     document.querySelector('#main-nav').style.opacity = 1;
-//   }
-// });
-
 // Smooth Scrolling
 $('#main-nav a, .btn').on('click', function (event) {
   if (this.hash !== '') {
