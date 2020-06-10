44195ef755c5c94a952859910c1307e43109ca8d
Afej <afekurojoshua@gmail.com>
2020-06-10 22:21:30 +0100

    stticky nav for desktop/tab

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#nav-wrapper').style.opacity = 0.9;
+  } else {
+    document.querySelector('#nav-wrapper').style.opacity = 1;
+  }
+});
+
 // Smooth Scrolling
 $('#main-nav a, .btn').on('click', function (event) {
   if (this.hash !== '') {
@@ -7,7 +16,7 @@ $('#main-nav a, .btn').on('click', function (event) {
 
     $('html, body').animate(
       {
-        scrollTop: $(hash).offset().top - 20,
+        scrollTop: $(hash).offset().top - 80,
       },
       800
     );
